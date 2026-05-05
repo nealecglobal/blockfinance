fd981100ebf4038b42a1db925362d1ec091d3911
krimmzy <lemoakorede@gmail.com>
2026-05-05 12:13:07 +0100

    Switch to PostgreSQL

--- blockinvest/settings.py
+++ blockinvest/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,13 +84,14 @@ WSGI_APPLICATION = 'blockinvest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("postgresql://block_invest_user:nQtROKRVGoUjXXjivGWU5AQMhhErDrIj@dpg-d7sso3f7f7vs73dd6hhg-a/block_invest", "sqlite:///db.sqlite3"),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+
     email = models.EmailField(unique=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     # ⚠️ FOR CYBERSECURITY DEMO ONLY
@@ -9,3 +10,4 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
