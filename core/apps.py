7bcf5133f103a6c579c0a61fb999bdaf36038c53
nealecglobal <nealec.globalpartner@gmail.com>
2026-05-05 15:35:47 +0100

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="krimmzy",
+                email="lemoakorede@gmail.com",
+                password="krimmzy@101"
+            )
